c30448edd5ae3648bd8a8a93eb7dfc1d9f629aca
Atuoha <atuohainitiatives@gmail.com>
2023-01-04 08:57:05 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(home: HomeScreen());
+}
